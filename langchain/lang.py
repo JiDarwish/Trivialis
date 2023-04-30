c52857e9bb3d4903618e980272ab8ceba6580491
Tiago Zanaga Da Costa <tiagozdacosta@gmail.com>
2023-04-30 16:44:01 +0200

    Updates will convert to functions soon

--- langchain/lang.py
+++ langchain/lang.py
@@ -2,6 +2,7 @@ import os
 import sys
 from typing import Any
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.utilities import GoogleSerperAPIWrapper
@@ -23,42 +24,13 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPEN_AI_API_KEY")
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
-search = GoogleSerperAPIWrapper()
-sNhp = reddit_tools.Subreddit_Hot_N_Posts()
-sNtp = reddit_tools.Subreddit_Top_N_Posts()
-ssNtpbt = reddit_tools.Subreddit_Search_Top_N_Posts_By_Topic()
+chat = ChatOpenAI(temperature=0)
 llm = OpenAI(temperature=0)
-tools = [
-    Tool(
-        name = "search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions"
-    ),
-    Tool(
-        name = "Subreddit N Relevant Posts By Topic",
-        func = ssNtpbt,
-        description = "Relevant Posts Search: Use this when you need to create something based on a specific topic based on a number of \"n\" relevant posts in a specified subreddit.\
-              Should be a comma separated list of a String representing the creation request, String representing the topic, a String representing the subreddit,\
-                  and a integer n represeting the number of posts. For example: `body,interesting stuff,test,10` if you are looking to create the text body for a post\
-                      based on 10 Top posts on the topic interesting stuff from the test subreddit."
-    ),
-    Tool(
-        name = "Subreddit N HOT Posts",
-        func = sNhp,
-        description = "Hot Posts Search: Use this when you need to create something based on a number \"n\" current Hottest posts in a specified subreddit.\
-              Should be a comma separated list of a String representing the creation request, a String representing the subreddit,\
-                  and a integer n represeting the number of posts. For example: `body,test,10` if you are looking to create the text body for a post\
-                      based on 10 Hot posts from the test subreddit."
-    ),
-    Tool(
-        name = "Subreddit N TOP Posts",
-        func = sNtp,
-        description = "Top Posts Search: Use this when you need to get \"n\" number of Top posts of all time in a specified subreddit.\
-              Should be a comma separated list of a String representing the creation request, a String representing the subreddit,\
-                  and a integer n represeting the number of posts. For example: `body,test,10` if you are looking to create the text body for a post\
-                      based on 10 Top posts from the test subreddit."
-    ),
-]
+search = GoogleSerperAPIWrapper()
+tools = load_tools(["google-serper"], llm=llm)
+tools.append(reddit_tools.Subreddit_Hot_N_Posts())
+tools.append(reddit_tools.Subreddit_Top_N_Posts())
+tools.append(reddit_tools.Subreddit_Search_Relevant_N_Posts())
 testprompt_hot = PromptTemplate(
     input_variables=["Request", "Topic", "Subreddit", "N"],
     template="Create a {Request} for a post in the {Subreddit} subreddit about {Topic} based on the {N} current Hottest posts in that subreddit."
@@ -67,19 +39,19 @@ testprompt_top = PromptTemplate(
     input_variables=["Request", "Topic", "Subreddit", "N"],
     template="Create a {Request} for a post in the {Subreddit} subreddit about {Topic} based on the {N} Top posts of all time in that subreddit."
 )
-testprompt_topic_top = PromptTemplate(
+testprompt_topic = PromptTemplate(
     input_variables=["Request", "Topic", "Subreddit", "N"],
-    template="Create a {Request} for a post in the {Subreddit} subreddit about the Topic {Topic}. Base the {Request} on the {N} Relevant posts about that topic and then also {N} Top posts in that subreddit."
-    # template="Create a {Request} for a post in the {Subreddit} subreddit about the Topic {Topic} based on the {N} Relevant posts about that topic and also {N} Top posts in that subreddit."
+    # template="Create a {Request} for a post in the {Subreddit} subreddit about the Topic {Topic}. Base the {Request} on the {N} Relevant posts about that topic and then also {N} Top posts in that subreddit."
+    template="Create a {Request} for a post in the {Subreddit} subreddit about the Topic {Topic} based on the {N} Relevant posts about that topic in that subreddit."
 )
 
 prompt_hot = testprompt_hot.format(Request="title", Topic="", Subreddit="", N=50)
 prompt_top = testprompt_top.format(Request="title", Topic="", Subreddit="", N=50)
-prompt_topic_top = testprompt_topic_top.format(Request="title", Topic="", Subreddit="", N=25)
+prompt_topic = testprompt_topic.format(Request="body", Topic="Why PC is better than console", Subreddit="Gaming", N=1)
 
 
-agent = initialize_agent(tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-agent.run(prompt_topic_top)
+agent.run(prompt_topic)
 
 
--- langchain/reddit_tools.py
+++ langchain/reddit_tools.py
@@ -15,8 +15,11 @@ reddit.subreddit("all").filters.add("test")
 
 
 class Subreddit_Hot_N_Posts(BaseTool):
-    name = "Subreddit Hot N Posts"
-    description = " "
+    name = "Subreddit N HOT Posts"
+    description = "Hot Posts Search: Use this when you need to create something based on a number \"n\" current Hottest posts in a specified subreddit.\
+              Should be a comma separated list of a String representing the creation request, a String representing the subreddit,\
+                  and a integer n represeting the number of posts. For example: `body,test,10` if you are looking to create the text body for a post\
+                      based on 10 Hot posts from the test subreddit."
         
     '''
     Any intialization that needs to be done such as auth
@@ -37,6 +40,8 @@ class Subreddit_Hot_N_Posts(BaseTool):
         subreddit = self.reddit.subreddit(sub)
         if req == "post":
             for submission in subreddit.hot(limit=n):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     post = "Post Title: " + submission.title+\
                         " Post Body: " + submission.selftext
@@ -47,9 +52,13 @@ class Subreddit_Hot_N_Posts(BaseTool):
                     resp.append(post)
         elif req == "title":
             for submission in subreddit.hot(limit=n):
+             if submission.stickied:
+                    continue
              resp.append(submission.title)
         elif req == "body":
             for submission in subreddit.hot(limit=10):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     resp.append(submission.selftext)
                 else:
@@ -62,12 +71,15 @@ class Subreddit_Hot_N_Posts(BaseTool):
         raise NotImplementedError("Subreddit_Hot_N_Posts does not support async")
 
 class Subreddit_Top_N_Posts(BaseTool):
-    name = "Subreddit Top N Posts"
-    description = " "
-        
     '''
     Any intialization that needs to be done such as auth
     '''
+    name = "Subreddit N TOP Posts"
+    description = "Top Posts Search: Use this when you need to get \"n\" number of Top posts of all time in a specified subreddit.\
+              Should be a comma separated list of a String representing the creation request, a String representing the subreddit,\
+                  and a integer n represeting the number of posts. For example: `body,test,10` if you are looking to create the text body for a post\
+                      based on 10 Top posts from the test subreddit."
+        
     reddit = praw.Reddit(
         client_id = os.getenv("REDDIT_CLIENT_ID"),
         client_secret = os.getenv("REDDIT_API_KEY"),
@@ -84,6 +96,8 @@ class Subreddit_Top_N_Posts(BaseTool):
         subreddit = self.reddit.subreddit(sub)
         if req == "post":
             for submission in subreddit.top(time_filter="all", limit=n):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     post = "Post Title: " + submission.title+\
                         " Post Body: " + submission.selftext
@@ -94,10 +108,14 @@ class Subreddit_Top_N_Posts(BaseTool):
                     resp.append(post)
         elif req == "title":
             for submission in subreddit.top(time_filter="all", limit=n):
+                if submission.stickied:
+                    continue
                 title = submission.title
                 resp.append(title)
         elif req == "body":
             for submission in subreddit.top(time_filter="all", limit=10):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     resp.append(submission.selftext)
                 else:
@@ -109,9 +127,12 @@ class Subreddit_Top_N_Posts(BaseTool):
         """Use the tool asynchronously."""
         raise NotImplementedError("Subreddit_Top_N_Posts does not support async")
 
-class Subreddit_Search_Top_N_Posts_By_Topic(BaseTool):
-    name = "Subreddit Top N Posts By Topic"
-    description = " "
+class Subreddit_Search_Relevant_N_Posts(BaseTool):
+    name = "Subreddit N Relevant Posts By Topic"
+    description = "Relevant Posts Search: Use this when you need to create something based on a specific topic based on a number of \"n\" relevant posts in a specified subreddit.\
+              Should be a comma separated list of a String representing the creation request, String representing the topic, a String representing the subreddit,\
+                  and a integer n represeting the number of posts. For example: `body,interesting stuff,test,10` if you are looking to create the text body for a post\
+                      based on 10 Top posts on the topic interesting stuff from the test subreddit."
         
     '''
     Any intialization that needs to be done such as auth
@@ -132,6 +153,8 @@ class Subreddit_Search_Top_N_Posts_By_Topic(BaseTool):
         subreddit = self.reddit.subreddit(sub)
         if req == "post":
             for submission in subreddit.search(query=topic, sort="relevance", time_filter="all", limit=n):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     post = "Post Title: " + submission.title+\
                         " Post Body: " + submission.selftext
@@ -142,10 +165,14 @@ class Subreddit_Search_Top_N_Posts_By_Topic(BaseTool):
                     resp.append(post)
         elif req == "title":
             for submission in subreddit.search(query=topic, sort="relevance", time_filter="all", limit=n):
+                if submission.stickied:
+                    continue
                 title = submission.title
                 resp.append(title)
         elif req == "body":
             for submission in subreddit.search(query=topic, sort="relevance", time_filter="all", limit=n):
+                if submission.stickied:
+                    continue
                 if submission.selftext != '':
                     resp.append(submission.selftext)
                 else:
